2c577b3f05b47cac6a5d03145f73c388174f9210
mac <Tunji32343234>
2019-03-16 13:26:20 +0100

    committing

--- Store/src/app/app-routing.module.ts
+++ Store/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SellProductComponent } from './sell-product/sell-product.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'selling', component: SellProductComponent},
+  {path: 'dashBoard', component: DashBoardComponent},
+  {path: 'products', component: ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Store/src/app/app.module.ts
+++ Store/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ProductsComponent } from './products/products.component';
+import { SellProductComponent } from './sell-product/sell-product.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashBoardComponent,
+    CreateProductComponent,
+    ProductsComponent,
+    SellProductComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
